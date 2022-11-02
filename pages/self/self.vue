d13390b0539bd15149d75b2aaacc17772eb90f7d
maowei <910479623@qq.com>
2022-11-02 11:36:33 +0800

    初始化配置信息1.0

---
+++ pages/self/self.vue
@@ -0,0 +1,22 @@
+<template>
+	<view>
+		self page
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			}
+		},
+		methods: {
+			
+		}
+	}
+</script>
+
+<style>
+
+</style>
